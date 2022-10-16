f4bacf83cf3ccc5f6ed3ce62e4b3af17092dec70
«Sheluto» <sheluto@gmail.com>
2022-10-17 00:23:55 +0300

    Массивы и метод

---
+++ Example010_HelloArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {18,32,36,47,18,62,74,18};
+
+int n = array.Length;   //позволяет получить длину(кол-во элементов) массива
+int find = 18;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //оператор прерывания
+    }
+    //index = index + 1;
+    index++;
+}
